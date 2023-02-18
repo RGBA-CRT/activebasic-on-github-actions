62c1c150ec078eb256d9c6c0ad9b7ad1c534f600
RGBA-CRT <rgba3crt1p@gmail.com>
2023-02-19 06:07:01 +0900

    test6

--- build.ps1
+++ build.ps1
@@ -1,7 +1,7 @@
-timeout 3
+ping -n 3 127.0.0.1 
 start powershell ".\screenshot.ps1 test3.png"
 start powershell ".\stracent.exe -f stFilter.txt .\.build\ActiveBasic\BasicCompiler.exe helloworld_rgbalib.abp ./build/helloworld_rgbalib.exe /wnd:0"
 start powershell ".\screenshot.ps1 test4.png"
-timeout 3
+timping -n 3 127.0.0.1 
 start powershell ".\screenshot.ps1 test5.png"
-timeout 10
+timetimping -n 10  127.0.0.1 
