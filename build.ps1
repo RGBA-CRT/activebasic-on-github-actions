fb9f32f631c63a7092ad3ce0246d1c27defdfee1
RGBA-CRT <rgba3crt1p@gmail.com>
2023-02-19 06:26:28 +0900

    test9

--- build.ps1
+++ build.ps1
@@ -1,8 +1,8 @@
 ping -n 1 127.0.0.1 
-start powershell ".\screenshot.ps1 test3.png"
-.\stracent.exe -f stFilter.txt .\.build\ActiveBasic\BasicCompiler.exe helloworld_rgbalib.abp ./build/helloworld_rgbalib.exe /wnd:0
+start powershell -WindowStyle Minimized  ".\screenshot.ps1 test3.png"
+.\.build\ActiveBasic\BasicCompiler.exe helloworld_rgbalib.abp ./build/helloworld_rgbalib.exe /wnd:0
 .\.build\ActiveBasic\ProjectEditor.exe
-start powershell ".\screenshot.ps1 test4.png"
+start powershell -WindowStyle Minimized  ".\screenshot.ps1 test4.png"
 ping -n 3 127.0.0.1 
-start powershell ".\screenshot.ps1 test5.png"
+start powershell -WindowStyle Minimized  ".\screenshot.ps1 test5.png"
 ping -n 1  127.0.0.1 
