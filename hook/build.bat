7bd2e6a90b4585e63a698e940e9fc7a5790348ce
RGBA-CRT <rgba3crt1p@gmail.com>
2023-03-05 12:55:36 +0900

    test 20

--- hook/build.bat
+++ hook/build.bat
@@ -1,2 +1,3 @@
-gcc -shared -o HookActiveBasicCompiler.dll HookActiveBasicCompiler.c -Wl,--kill-at -lDbghelp 
+gcc -shared -o HookActiveBasicCompiler.dll HookGuiEvents.c -Wl,--kill-at -lDbghelp 
+@REM gcc -shared -o HookActiveBasicCompiler.dll HookActiveBasicCompiler.c -Wl,--kill-at -lDbghelp 
 gcc runABCompilerAsCLI.c -o runABCompilerAsCLI.exe -lshlwapi
